fff9943b62aa1821038d9c154228c863e09fb7b6
mitcaizi <jling1@mail.csuchico.edu>
2018-03-01 07:32:19 -0800

    play

--- main.cpp
+++ main.cpp
@@ -5,7 +5,28 @@ using namespace std;
 
 int main()
 {
-  SecretDoor game;
-    
-  return 0;
+        SecretDoor game;
+        int repeat;
+        int win=0;
+        cout<<"How many times do you want to play?"<<endl;
+        cin>>repeat;
+        for (int x=0; x<repeat; x++)
+        {
+                game.newGame();
+                game.guessDoorC();
+                game.guessDoorC();
+                if (game.isWinner() == true)
+                {
+                        win++;
+                }
+        }
+        if ( win<=1)
+        {
+        cout<<"You won "<<win<< " time "<<endl;
+        }
+        else
+        {
+        cout<<"You won "<<win<<" times "<<endl;
+        }
+        return 0;
 }
